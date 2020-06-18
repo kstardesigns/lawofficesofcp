d9dc49adacc68bf01cb21ef7a43fb5f2d9b51f3e
Kyle Stark <kyleastark@gmail.com>
2020-06-18 16:12:06 -0700

    Continued styling global contact form, jquery to move form labels on focus

--- js/kyle-scripts.js
+++ js/kyle-scripts.js
@@ -1,1 +1,6 @@
-//scripts coming soon
+jQuery(document).ready(function($) {
+    //move form field label when clicked
+    $('.elementor-field').focus(function() {
+        $(this).siblings('.elementor-field-label').addClass('with-text');
+    });
+});
